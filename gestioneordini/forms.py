0e5de16dbe2b33bcbe3f4261134144d72e580fcb
r4gtl <ferrari.stefano75@gmail.com>
2022-08-29 19:06:01 +0200

    Time picker problems

--- gestioneordini/forms.py
+++ gestioneordini/forms.py
@@ -41,7 +41,7 @@ class TempoModelForm(forms.ModelForm):
             # "datatempo": forms.DateInput(attrs={'class':'form-control', 'value': datetime.date.today().strftime("%d-%m-%Y"), 'type': 'date'}),
             "idtempo": forms.HiddenInput(),
             "datatempo": forms.DateInput(format=('%Y-%m-%d'), attrs={'class':'form-control', 'type': 'date'}),
-            "orainizio": forms.TimeInput(format=('%H:%M:%S'),attrs={'type': 'time'}),
+            "orainizio": forms.TimeInput(format=('%H:%M:%S'),attrs={'type': 'time', 'id': 'id_date_select'}),
             "orafine": forms.TimeInput(format=('%H:%M:%S'), attrs={'type': 'time'}),            
             "note": forms.Textarea(attrs={'cols': 80, 'rows': 2}),
             # "idoperatore": forms.ChoiceField(choices = Tbloperatori.objects.filter(dimesso__iexact="false").exclude(tbltempi__orafine__isnull=True).order_by('cognome'), initial=0)
